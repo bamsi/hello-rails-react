a2ad1e70e9d4c7d8423176db676f808c354d7105
bamsi <hibamsi@gmail.com>
2022-10-04 15:49:00 +0300

    add rubocop file for linters

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
-# frozen_string_literal: true
-
-Rails.application.routes.draw do  
+Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "root#index"
+  root 'root#index'
   resources :greetings, only: [:index]
 end
