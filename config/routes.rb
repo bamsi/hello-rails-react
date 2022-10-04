d6a11457026f149f5d5be6c7e5b1973e2a6bc86f
bamsi <hibamsi@gmail.com>
2022-10-04 12:10:31 +0300

    add greeting route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-Rails.application.routes.draw do
-  root 'root#index'
+Rails.application.routes.draw do  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "root#index"
+  resources :greetings, only: [:index]
 end
